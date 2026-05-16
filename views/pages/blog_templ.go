2c58f1c8252e50334bdd673193993453f11c668f
brian-nunez <bnunez9999@gmail.com>
2026-05-16 16:45:40 -0700

    chore(ui): improvements

--- views/pages/blog_templ.go
+++ views/pages/blog_templ.go
@@ -308,7 +308,7 @@ func Blog(data BlogViewData) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 29, "<script src=\"/assets/js/blog-page.js\"></script></body>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 29, "<script src=\"/assets/js/blog-page.js?v=1.0.0\"></script></body>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
